83c252fcf08b5f5efd0cf6e952c9dfa1079c9567
said-louham <111187783+said-louham@users.noreply.github.com>
2024-06-22 22:49:10 +0100

    add reservation routes

---
+++ backend-laravel/database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+      $adminList = [
+            ['name' => 'SuperAdmin', 'email' => 'superadmin@gmail.com'],
+            ['name' => 'hickma', 'email' => 'hickma@gmail.com'],
+        ];
+
+        foreach ($adminList as $admin) {
+            $user = User::firstOrCreate(
+                ['name' => $admin['name']],
+                ['email' => $admin['email'], 'password' =>'12345678']
+            );
+
+        }
+    }
+}
