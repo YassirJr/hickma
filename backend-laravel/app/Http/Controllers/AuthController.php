ff3403ceb734d6d311cc9f027dd175ff0c9d3430
said-louham <111187783+said-louham@users.noreply.github.com>
2024-06-22 21:07:14 +0100

    add auth

---
+++ backend-laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class AuthController extends Controller
+{
+
+    public function register(RegisterUserRequest $request)
+    {
+
+        $data = $request->validated();
+
+        DB::beginTransaction();
+
+        User::create($data);
+
+        DB::commit();
+
+        return response()->json(true);
+    }
+
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (!auth()->attempt($data)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        $tokenName = config('APP_NAME', 'TOKEN_NAME');
+        $token = $request->user()->createToken($tokenName, ['*']);
+
+        return response()->json([
+            'token' => $token->plainTextToken,
+            'user' => [
+                'name' => $request->user()->name,
+                'role' => $request->user()->role
+            ]
+        ]);
+    }
+
+    public function logout()
+    {
+        $user = request()->user();
+        $user->tokens()->where('id', $user->currentAccessToken()->id)->delete();
+
+        return response()->json(true);
+    }
+
+    public function user()
+    {
+        $user = Auth::user();
+        $user = $user->only(['id', 'email', 'role']);
+
+        return response()->json($user);
+    }
+
+}
